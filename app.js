af0e5cc9b4e26b0148fd0ae3d0ef84b000eb943d
Ryan <ryan@ryanwans.com>
2020-12-08 21:16:15 -0500

    automated

--- app.js
+++ app.js
@@ -535,6 +535,9 @@ app.post("/analytics/*", function(req, res) {
   var t = req.body;
   if("object" != typeof t) {t = JSON.parse(t);}
   console.log(t, req.body);
+  setTimeout(function() {
+    console.log(t);
+  },1000);
   var TransBuffer = Buffer.from(t.pack);
   var Analytics = JSON.parse(TransBuffer.toString());
 
